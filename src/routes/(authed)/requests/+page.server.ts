0d5838df94c768b294fe82f79b7a13ee57f82fc2
Dries Meirte <dries.meirte@optis.be>
2025-03-14 16:41:33 +0000

    Remove denied requests from budget calculation

--- src/routes/(authed)/requests/+page.server.ts
+++ src/routes/(authed)/requests/+page.server.ts
@@ -3,6 +3,7 @@ import { db } from '$lib/server/db';
 import { trainingRequestTable, trainingTable } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
 import type { LayoutServerLoad } from '../$types';
+import {REQUEST_STATES} from "../../../lib/server/db/schema";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const trainingRequests = await db
@@ -19,11 +20,13 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 		.from(trainingRequestTable)
 		.leftJoin(trainingTable, eq(trainingRequestTable.trainingId, trainingTable.id))
 		.where(eq(trainingRequestTable.userId, locals.user!.id));
-	const usedBudget = trainingRequests.reduce(
-		(acc, { durationDays, ticketCost }) =>
-			acc + (parseFloat(durationDays) * 500 + (ticketCost ? Number(ticketCost) : 0)),
-		0
-	);
+	const usedBudget = trainingRequests
+		.filter(request => request.status != REQUEST_STATES[2])
+		.reduce(
+			(total, { durationDays, ticketCost }) =>
+				total + (parseFloat(durationDays) * 500 + (ticketCost ? Number(ticketCost) : 0)),
+			0
+		);
 
 	const availableBudget = TOTAL_BUDGET - usedBudget;
 	return { trainingRequests, availableBudget, usedBudget };
