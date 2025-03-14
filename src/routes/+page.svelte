6d7b9ef6f2480abda50bd725c1db3f83ba0c6f7a
Charlotte Van der Voort <charlotte.vandervoort@optis.be>
2025-03-14 12:38:26 +0000

    added create event button in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import ConferenceCard from '../lib/components/ConferenceCard.svelte';
+	import { goto } from '$app/navigation';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 </script>
 
 <div class="container mx-auto p-4">
-	<h1 class="mb-8 text-center text-4xl font-bold">Upcoming Developer Conferences</h1>
+	<div class="flex">
+		<h1 class=" mb-8 grow text-center text-4xl font-bold">Upcoming Developer Conferences</h1>
+		<a class="btn btn-success" href="training/new">Create new event</a>
+	</div>
 
 	<div class="flex flex-wrap justify-center gap-4">
 		{#each data.trainings as conference}
